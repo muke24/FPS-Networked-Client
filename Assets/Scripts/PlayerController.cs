3ba52b5133ba64866bee7dcbf33e7224a9534b18
Peter <pjthompson.au@gmail.com>
2020-07-17 12:50:35 +1000

    FPS client

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿#region This code has been written by Peter Thompson
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Transform camTransform;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+			ClientSend.PlayerShoot(camTransform.forward);
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		SendInputToServer();
+	}
+
+	/// <summary>Sends player input to the server.</summary>
+	private void SendInputToServer()
+	{
+		bool[] _inputs = new bool[]
+		{
+			Input.GetKey(KeyCode.W),
+			Input.GetKey(KeyCode.S),
+			Input.GetKey(KeyCode.A),
+			Input.GetKey(KeyCode.D),
+			Input.GetKey(KeyCode.Space)
+		};
+
+		ClientSend.PlayerMovement(_inputs);
+	}
+}
+//* This code has been written by Peter Thompson
+#endregion
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿#region This code has been written by Peter Thompson
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	public static UIManager instance;
+
+	public GameObject startMenu;
+	public InputField usernameField;
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Debug.Log("Instance already exists, destroying object!");
+			Destroy(this);
+		}
+	}
+
+	/// <summary>Attempts to connect to the server.</summary>
+	public void ConnectToServer()
+	{
+		startMenu.SetActive(false);
+		usernameField.interactable = false;
+		Client.instance.ConnectToServer();
+	}
+}
+//* This code has been written by Peter Thompson
+#endregion
